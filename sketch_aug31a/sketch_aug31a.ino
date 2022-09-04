4517582590596e79abb6cfa41389668318de1bd8
Operfectlove <106957635+Operfectlove@users.noreply.github.com>
2022-09-04 14:29:48 +0900

    final

--- sketch_aug31a/sketch_aug31a.ino
+++ sketch_aug31a/sketch_aug31a.ino
@@ -1,19 +1,58 @@
-#include <SoftwareSerial.h>
- 
-#define BT_RXD 8
-#define BT_TXD 7
-SoftwareSerial bluetooth(BT_RXD, BT_TXD);
- 
-void setup(){
-  Serial.begin(9600);
-  bluetooth.begin(9600);
+#include <ESP8266WiFi.h>
+
+//esp8266 wemos wifi d1 mini board
+
+
+const char* ssid     = "iPhone"; // 사용 중 인 와이파이 이름
+const char* password = "qwerty1234"; // 와이파이 패스워드
+WiFiServer server(80);
+
+void setup() {
+  pinMode(A0,INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(12, OUTPUT);
+  Serial.begin(115200); // 시리얼 통신, 속도 115200
+  delay(10);
+  Serial.println();
+
+  // Connect to WiFi network
+  WiFi.mode(WIFI_STA);
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(100);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
+
+  // Print the IP address
+  Serial.println(WiFi.localIP());
 }
- 
-void loop(){
-  if (bluetooth.available()) {
-    Serial.write(bluetooth.read());
+
+void loop() {
+  delay(50);
+  WiFiClient client = server.available();
+  client.print("HTTP/1.1 200 OK");
+  client.print("Content-Type: text/html");
+  client.print("Refresh: 0.1");  // 자동으로 웹페이지 새로고침 (0.1초 설정)
+  String request = client.readStringUntil('\n');
+  Serial.println(request);
+  if(request.indexOf("GET") == -1){
+    Serial.println("offmask");
   }
-  if (Serial.available()) {
-    bluetooth.write(Serial.read());
+  else{
+    Serial.println("onmask");
   }
+
 }
